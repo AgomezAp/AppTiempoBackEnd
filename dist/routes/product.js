e9eceaa6536105d5e3760e441df2843e0d58e8e6
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2024-12-18 11:10:59 -0500

    validateToken agregado

--- dist/routes/product.js
+++ dist/routes/product.js
@@ -8,13 +8,13 @@ const product_1 = require("../controllers/product");
 const validateToken_1 = __importDefault(require("./validateToken"));
 const router = (0, express_1.Router)();
 // Ruta para registrar un nuevo producto
-router.post("/api/product/register", product_1.registerProduct);
+router.post("/api/product/register", validateToken_1.default, product_1.registerProduct);
 // Ruta para obtener todo el inventario (requiere validación de token)
 router.get("/api/product/ObtenerInventario", validateToken_1.default, product_1.getInventario);
 // Ruta para obtener un producto por ID
-router.get("/api/product/obtener/:id", product_1.getProductById);
+router.get("/api/product/obtener/:id", validateToken_1.default, product_1.getProductById);
 // Ruta para borrar un producto por ID
-router.delete("/api/product/delete/:id", product_1.deleteProductById);
+router.delete("/api/product/delete/:id", validateToken_1.default, product_1.deleteProductById);
 // Ruta para actualizar un producto por ID
-router.patch("/api/product/actualizar/:id", product_1.updateProductById);
+router.patch("/api/product/actualizar/:id", validateToken_1.default, product_1.updateProductById);
 exports.default = router;
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -12,18 +12,18 @@ import validateToken from './validateToken';
 const router = Router();
 
 // Ruta para registrar un nuevo producto
-router.post("/api/product/register", registerProduct);
+router.post("/api/product/register",validateToken ,registerProduct);
 
 // Ruta para obtener todo el inventario (requiere validación de token)
 router.get("/api/product/ObtenerInventario",validateToken, getInventario);
 
 // Ruta para obtener un producto por ID
-router.get("/api/product/obtener/:id",  getProductById);
+router.get("/api/product/obtener/:id", validateToken,getProductById);
 
 // Ruta para borrar un producto por ID
-router.delete("/api/product/delete/:id", deleteProductById);
+router.delete("/api/product/delete/:id",validateToken ,deleteProductById);
 
 // Ruta para actualizar un producto por ID
-router.patch("/api/product/actualizar/:id", updateProductById);
+router.patch("/api/product/actualizar/:id",validateToken, updateProductById);
 
 export default router;
