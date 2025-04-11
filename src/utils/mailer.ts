c37275ec7c7735c4c5984d1d29385d5c01c81945
Inge-AP <aagudelo.investigacion@andrespublicidadtg.com>
2025-04-11 15:04:10 -0500

    mailer

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer';
-
+import crypto from 'crypto'
 const transporter = nodemailer.createTransport({
   service: process.env.EMAIL_SERVICE, // Servicio de correo electrónico (por ejemplo, 'gmail')
   auth: {
@@ -9,15 +9,21 @@ const transporter = nodemailer.createTransport({
 });
 
 export const sendMail = (to: string[], subject: string, text: string) => {
+  const domain = process.env.EMAIL_SERVICE;
+  const messageId = `<${crypto.randomUUID()}@${domain}.com>`;
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: to.join(','),
     subject,
     text,
     headers: {
-      'References': '',
-      'In-Reply-to': '',
+      'Message-ID': messageId,
+      'X-Entity-Ref-ID': crypto.randomUUID(),
+      'Precedence': 'bulk',
+      'Auto-Submitted': 'auto-generated',
     },
+    inReplyTo: undefined,
+    references: undefined
   };
 
   return transporter.sendMail(mailOptions);
