43ea1ed4416116f03697ac27304f47c493e8ceb0
AgomezAp <agomez.desarrollo@andrespublicidadtg.com>
2026-03-17 11:39:30 -0500

    Remove dist/ and package-lock.json from repo, keep in .gitignore

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config"); // DEBE ser la primera línea - carga .env antes de todo
-const server_1 = __importDefault(require("./models/server"));
-require("./utils/cron");
-const server = new server_1.default();
--- dist/routes/permisos.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const permisos_1 = require("../controllers/permisos");
-const router = (0, express_1.Router)();
-router.post('/api/permisos/crear', permisos_1.createPermiso);
-router.get('/api/permisos/:id', permisos_1.getPermisosByUserId);
-router.get('/api/admin/users', permisos_1.getAllUsersWithPermisos);
-exports.default = router;
